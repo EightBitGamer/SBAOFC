869839747f4da2251743d5c6fb4518e219c1df72
EightBitGamer <eight.2nd@gmail.com>
2013-05-16 18:53:31 +1000

    bug fixes
    fixed error when removing existing object files
    fixed error when saving pre-made object files

--- SBAOFC.py
+++ SBAOFC.py
@@ -6,6 +6,29 @@ print "SB animated object files creator v1.4"
 
 values = ["objectName", "description", "shortDescription", "race", "dropItem", "category", "image", "imagePath", "imagePosition", "frames", "animationcycle", "spaceScan", "anchors", "size", ]
 
+def fileExists(objectNameCheck):
+	newNameValid = 0
+	while newNameValid == 0:
+		dotObjectFolder = curDir + "/" + objectNameCheck
+		if os.path.exists(dotObjectFolder):
+			print "Item already exists!"
+			newName = raw_input("Choose a different name (leave blank to overwrite)> ")
+			if raw_input("Are you sure you want to delete %s ? (Y/N)>" % objectName):
+				if newName == "":
+					files = os.listdir(dotObjectFolder)
+					for objectFile in files:
+						os.remove(dotObjectFolder + "/" + objectFile)
+					os.rmdir(dotObjectFolder)
+					dotObjitemLocation = AsItOb + "/" + objectNameCheck + ".frames"
+					objectIconPngLocation = AsItOb + "/" + objectNameCheck + "/" + objectNameCheck + "icon.png"
+					if os.path.exists(dotObjitemLocation):
+						os.remove(dotObjitemLoction)
+					if os.path.exists(objectIconPngLocation):
+						os.remove(objectDotIconPngLocation)
+		else:
+			newNameValid = None
+	return objectNameCheck
+
 def setValue(valueName, format, default)	:
 	value = None
 	while value == None:
@@ -32,82 +55,69 @@ def setValue(valueName, format, default)	:
 curDir = os.getcwd()
 AsItOb = curDir + "/" + "assets_items_object"
 
+imagePosition = [None,None]
+size = [None,None]
+names = []
+
 preMade = raw_input("Drag pre-made object file here or press enter to create new> ")
 if preMade != "":
 	try:
+		print "loading file"
 		loadedFile = open(preMade, "r")
 		objectName = loadedFile.readline()
+		objectName = fileExists(objectName)
 		description = loadedFile.readline()
 		shortDescription = loadedFile.readline()
 		race = loadedFile.readline()
 		dropItem = loadedFile.readline()
 		category = loadedFile.readline()
-		image = loadedFile.readline()
 		imagePath = loadedFile.readline()
 		imageIconPath = loadedFile.readline()
-		imagePosition = loadedFile.readline()
+		imagePosition[0] = loadedFile.readline()
+		imagePosition [1]= loadedFile.readline()
 		frames = loadedFile.readline()
 		animationCycle = loadedFile.readline()
 		spaceScan = loadedFile.readline()
 		anchors = loadedFile.readline()
-		size = loadedFile.readline()
+		size[0] = loadedFile.readline()
+		size[1] = loadedFile.readline()
 		names_all = loadedFile.readline()
 		rarity = loadedFile.readline()
 	except:
 		print "Could not load files"
 	preMade = ""
 else:
-	if 1 + 1 == 2:
-		objectName = setValue("Object Name", "String", "")
-		dotObjectFolder = curDir + "/" + objectName
-		if os.path.exists(dotObjectFolder):
-			print "Item already exists!"
-			if raw_input("Overwrite data? (Y/N)>") == "Y":
-				files = os.listdir(dotObjectFolder)
-				for objectFile in files:
-					os.remove(dotObjectFolder + "/" + objectFile)
-				os.rmdir(dotObjectFolder)
-				dotObjitemLocation = AsItOb + "/" + objectName + ".frames"
-				objectIconPngLocation = AsItOb + "/" + objectName + "/" + objectName + "icon.png"
-				if os.path.exists(dotObjitemLocation):
-					os.remove(dotObjitemLoction)
-				if os.path.exists(objectIconPngLocation):
-					os.remove(objectDotIconPngLocation)
-	else:
-		print "failed to remove files"
-		sys.exit()
+	objectName = setValue("Object Name", "String", "")
+	objectName = fileExists(objectName)
 	description = setValue("Description", "String", "")
 	shortDescription = setValue("Short Description", "String", "")
 	race = setValue("Race", "String", "Human")
 	dropItem = setValue("Drop Item", "String", objectName)
 	category = setValue("Category", "String", "decorative")
-	imagePosition = [None,None]
 	imagePosition[0] = setValue("X offset", "Decimal", 0)
 	imagePosition[1] = setValue("Y offset", "Decimal", 0)
 	frames = setValue("Frames", "Integer", 1)
-	print frames
 	animationCycle = setValue("Animation Cycle", "Decimal", 0.1)
 	spaceScan = setValue("Space Scan", "Decimal", 0.1)
 	anchors = setValue("Anchors", "String", "Background")
-	size = [None,None]
 	size[0] = setValue("Width", "Interger", 8)
 	size[1] = setValue("Height", "Interger", 8)
-	dimentions = [frames,1]
 	names_all = setValue("Frame names", "String", "default")
-	names = []
-	nameCount = 0
-	while nameCount < int(dimentions[0]) + 1:
-		names.append(names_all + str(nameCount))
-		nameCount = nameCount + 1
-	itemName = objectName
 	rarity = setValue("Rarity", "String", "common")
-	inventoryIcon = objectName + "icon.png"
 	imagePath = setValue("Tile sheet (WIP, leave blank)", "Image", " ")
 	imageIconPath =setValue("Inventory Icon (WIP, leave blank)", "Image", " ")
 
+dimentions = [frames,1]
+itemName = objectName
+inventoryIcon = objectName + "icon.png"
+nameCount = 0
+while nameCount < int(dimentions[0]) + 1:
+	names.append(names_all + str(nameCount))
+	nameCount = nameCount + 1
+
 print "Saving..."
 print "creating object folder"
-odotOjectFolder = curDir + "/" + objectName
+dotObjectFolder = curDir + "/" + objectName
 os.mkdir(dotObjectFolder)
 
 print "Formatting %s.object" % objectName
@@ -196,21 +206,15 @@ if imageIconPath != " ":
 	saveFile.write(originalFile)
 	saveFile.close
 print "Complete"
-if raw_input("create share file? (Y/N)> ") == "Y":
-	preMadeFile = objectName + "\n"
-	description + "\n"
-	shortDescription + "\n"
-	race + "\n"
-	dropItem + "\n"
-	category + "\n"
-	image + "\n"
-	imagePath + "\n"
-	imageIconPath + "\n"
-	imagePosition + "\n"
-	frames + "\n"
-	animationCycle + "\n"
-	spaceScan + "\n"
-	anchors + "\n"
-	size + "\n"
-	names_all + "\n"
-	rarity
+if raw_input("create Pre-made object file? (Y/N)> ") == "Y":
+	preMadeFile = objectName + "\n" + description + "\n" + shortDescription + "\n" + race + "\n" + dropItem + "\n" + category + "\n" + imagePath + "\n" + imageIconPath + "\n" + str(imagePosition[0]) + "\n" + str(imagePosition[1]) + "\n" + str(frames) + "\n" + str(animationCycle) + "\n" + str(spaceScan) + "\n" + anchors + "\n" + str(size[0]) + "\n" + str(size[1]) + "\n" + names_all + "\n" + rarity
+	print "Checking for Pre-made objects folder (pre-release)"
+	preMadeFolder = curDir + "/premade"
+	if not os.path.exists(preMadeFolder):
+		print "Folder not found\nCreating folder"
+		os.mkdir(preMadeFolder)
+	else:
+		print "Folder found"
+	saveFile = open(preMadeFolder + "/" + objectName + ".aof", "w+")
+	saveFile.write(preMadeFile)
+	saveFile.close
